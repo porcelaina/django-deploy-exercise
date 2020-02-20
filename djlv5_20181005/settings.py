6d125dbf6a4802193d23e6e086942f5945387ff9
sidewind on HP notebook <be-here-now@hanmail.net>
2020-02-20 22:02:28 +0400

    remove staticfiles from installed_apps

--- djlv5_20181005/settings.py
+++ djlv5_20181005/settings.py
@@ -41,9 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'basic_app5'
+    'django.contrib.messages',    
+    'basic_app5',
 ]
 
 MIDDLEWARE = [
